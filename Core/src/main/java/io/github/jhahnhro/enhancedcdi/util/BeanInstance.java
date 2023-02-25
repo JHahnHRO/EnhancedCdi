e7b819f44ab37ff8c3604eef8242275f7e38a5e0
Johannes Hahn <johanneshahnrostock@googlemail.com>
2023-02-25 02:00:57 +0100

    make BeanInstance lazy and rewrite BeanHelper to automatically take care of the destruction of dependent beans

--- Core/src/main/java/io/github/jhahnhro/enhancedcdi/util/BeanInstance.java
+++ Core/src/main/java/io/github/jhahnhro/enhancedcdi/util/BeanInstance.java
@@ -1,13 +1,14 @@
 package io.github.jhahnhro.enhancedcdi.util;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.function.Supplier;
 import javax.enterprise.context.spi.Contextual;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.InjectionPoint;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import java.util.Set;
 
 /**
  * Convenience object encapsulating a bean, one of its instances and the {@link CreationalContext} in which that
@@ -16,41 +17,46 @@ import java.util.Set;
  * According to the CDI Spec a bean has:
  * <ol>
  *     <li><i>contextual instances</i>, i.e. the "real" instance(s) of the bean. There can be one or more,
- *     depending on the bean's scope, i.e. one for {@code @ApplicationScoped} and {@code @Singleton}
- *     beans, as many as there are requests for {@code @RequestScoped}, an unlimited amount for {@code @Dependent}
- *     etc. Calling methods on the contextual instance(s) directly calls the "real" method in the bean class.</li>
- *     <li><i>contextual references</i>, i.e. client proxies around the contextual instances for all normal scoped
- *     beans. The client proxies also include the interceptor chain, i.e. calling methods on a contextual reference
- *     goes through the complete interceptor and decorator chain of the bean before arriving at the "real", i.e. the
- *     contextual instance.</li>
- *     <li><i>injectable references</i> for each injection point that resolves to the bean.</li>
- *     <li>{@link javax.enterprise.inject.spi.Unmanaged unmanaged instances} that support interceptors, decorators,
- *     lifecycle callbacks etc., but are not managed by the CDI container and live independently along-side the
- *     managed instance(s).</li>
+ *     depending on the bean's scope:
+ *     <ul>
+ *         <li>one for {@code @ApplicationScoped} and {@code @Singleton} beans,</li>
+ *         <li>as many as there are requests for {@code @RequestScoped} beans,</li>
+ *         <li>a potentially unlimited amount for {@code @Dependent} scoped beans</li>
+ *         <li>...</li>
+ *     </ul>
+ *     Calling methods on the contextual instance(s) directly calls the "real" method in the bean class, i.e.
+ *     interceptors and decorators are not applied.</li>
+ *     <li><i>contextual references</i> which are usually client proxies wrapped around the contextual instances. For
+ *     all normal scoped beans, they are always client proxies, for {@code @Dependent} scoped beans they may be
+ *     proxies or not. The client proxies also include the interceptor chain if necessary, i.e. calling methods on a
+ *     contextual reference goes through the complete interceptor and decorator chain of the bean before arriving at
+ *     the "real", i.e. the contextual instance.</li>
+ *     <li><i>injectable references</i> for each injection point that resolves to the bean. These are also usually
+ *     client proxies.</li>
  * </ol>
- *
- * @param instance
- * @param contextual
- * @param context
- * @param <T>
  */
-public record BeanInstance<T>(T instance, Contextual<T> contextual, CreationalContext<T> context) {
+public final class BeanInstance<T> {
+    private final Supplier<T> instanceSupplier;
+    private final Contextual<T> contextual;
+    private final CreationalContext<T> context;
+    private T instance;
+    private State state;
 
-    /**
-     * Creates a contextual instance by calling {@link Contextual#create(CreationalContext)}.
-     *
-     * @param contextual
-     * @param context
-     * @param <T>
-     * @return
-     */
+    private BeanInstance(Supplier<T> instanceSupplier, Contextual<T> contextual, CreationalContext<T> context) {
+        this.instanceSupplier = instanceSupplier;
+        this.contextual = contextual;
+        this.context = context;
+        this.instance = null;
+        this.state = State.NOT_INITIALIZED;
+    }
+
     public static <T> BeanInstance<T> createContextualInstance(Contextual<T> contextual, CreationalContext<T> context) {
-        return new BeanInstance<>(contextual.create(context), contextual, context);
+        return new BeanInstance<>(() -> contextual.create(context), contextual, context);
     }
 
     public static <T> BeanInstance<T> createContextualReference(BeanManager beanManager, Bean<T> bean, Type beanType) {
         final CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
-        return new BeanInstance<>((T) beanManager.getReference(bean, beanType, ctx), bean, ctx);
+        return new BeanInstance<>(() -> (T) beanManager.getReference(bean, beanType, ctx), bean, ctx);
     }
 
     public static <T> BeanInstance<T> createInjectableReference(BeanManager beanManager,
@@ -59,10 +65,36 @@ public record BeanInstance<T>(T instance, Contextual<T> contextual, CreationalCo
         final Set<Bean<?>> beans = beanManager.getBeans(injectionPoint.getType(), qualifiers);
         final Bean<T> resolvedBean = (Bean<T>) beanManager.resolve(beans);
         final CreationalContext<T> ctx = beanManager.createCreationalContext(resolvedBean);
-        return new BeanInstance<>((T) beanManager.getInjectableReference(injectionPoint, ctx), resolvedBean, ctx);
+        return new BeanInstance<>(() -> (T) beanManager.getInjectableReference(injectionPoint, ctx), resolvedBean, ctx);
     }
 
-    public void destroy() {
-        contextual.destroy(instance, context);
+    public synchronized void destroy() {
+        if (state == State.INITIALIZED) {
+            contextual.destroy(instance, context);
+            state = State.DESTROYED;
+        }
+    }
+
+    public synchronized T instance() {
+        if (state == State.NOT_INITIALIZED) {
+            instance = instanceSupplier.get();
+            state = State.INITIALIZED;
+        }
+        return instance;
+    }
+
+    public Contextual<T> contextual() {return contextual;}
+
+    public CreationalContext<T> context() {return context;}
+
+    /**
+     * @return current state of this instance
+     */
+    public synchronized State state() {
+        return state;
+    }
+
+    public enum State {
+        NOT_INITIALIZED, INITIALIZED, DESTROYED
     }
 }
--- Core/src/test/java/io/github/jhahnhro/enhancedcdi/util/BeanHelperTest.java
+++ Core/src/test/java/io/github/jhahnhro/enhancedcdi/util/BeanHelperTest.java
@@ -1,34 +1,44 @@
 package io.github.jhahnhro.enhancedcdi.util;
 
-import org.jboss.weld.junit5.WeldInitiator;
-import org.jboss.weld.junit5.WeldJunit5Extension;
-import org.jboss.weld.junit5.WeldSetup;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import javax.enterprise.inject.Any;
-import javax.enterprise.inject.Default;
-import javax.enterprise.inject.Produces;
-import javax.enterprise.util.AnnotationLiteral;
-import javax.enterprise.util.TypeLiteral;
-import javax.inject.Inject;
-import javax.inject.Qualifier;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Type;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.AmbiguousResolutionException;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.UnsatisfiedResolutionException;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.enterprise.util.TypeLiteral;
+import javax.inject.Inject;
+import javax.inject.Qualifier;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.inject.WeldInstance;
+import org.jboss.weld.junit5.EnableWeld;
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@ExtendWith(WeldJunit5Extension.class)
 class BeanHelperTest {
 
     @Produces
@@ -46,12 +56,13 @@ class BeanHelperTest {
     @Produces
     @MyQualifier
     static Map<Integer, String> qualifiedMap = Map.of(1, "-1", 2, "-2", 3, "-3");
-    @WeldSetup
-    WeldInitiator weld = WeldInitiator.from(BeanHelper.class, BeanHelperTest.class).build();
+    Weld w = new Weld().disableDiscovery()
+            .addBeanClasses(BeanHelper.class, BeanHelperTest.class, AppScopedBean.class, DependentBean.class);
+
     @Inject
     BeanHelper beanHelper;
 
-    public static Stream<Arguments> getArguments() {
+    static Stream<Arguments> getArguments() {
         final Annotation[] none = new Annotation[0];
         return Stream.of(
                 //@formatter:off
@@ -65,27 +76,157 @@ class BeanHelperTest {
                 arguments(new TypeLiteral<Map<Integer, String>>() {}.getType(), new Annotation[]{Any.Literal.INSTANCE},
                           new Map[]{defaultMap, qualifiedMap}),
                 arguments(new TypeLiteral<Map<Integer, String>>() {}.getType(),
-                          new Annotation[]{new AnnotationLiteral<MyQualifier>() {}}, new Map[]{qualifiedMap})
+                          new Annotation[]{new AnnotationLiteral<MyQualifier>() {}}, new Map[]{qualifiedMap}),
+                arguments(new TypeLiteral<Map<Integer, String>>() {}.getType(),
+                          new Annotation[]{new AnnotationLiteral<MyOtherQualifier>() {}}, new Map[0])
                 //@formatter:on
         );
     }
 
-    @ParameterizedTest
-    @MethodSource("getArguments")
-    <T> void testSelect(final Type beanType, Annotation[] qualifiers, T[] expectedInstances) {
-        final Collection<BeanInstance<T>> beanInstances = beanHelper.select(beanType, qualifiers);
-
-        final Collection<T> actual = beanInstances.stream().map(BeanInstance::instance).toList();
-        assertThat(actual).containsExactlyInAnyOrder(expectedInstances);
+    @BeforeEach
+    void resetCounters() {
+        AppScopedBean.destroyed = 0;
+        DependentBean.destroyed = 0;
     }
 
     @Qualifier
     @Retention(RetentionPolicy.RUNTIME)
+    private @interface MyOtherQualifier {}
+
+    interface MyInterface {}
+
+    @Qualifier
+    @Retention(RetentionPolicy.RUNTIME)
     private @interface MyQualifier {}
+
+    @ApplicationScoped
+    static class AppScopedBean implements MyInterface {
+        static int destroyed = 0;
+
+        @PreDestroy
+        void destroy() {
+            destroyed++;
+        }
+    }
 
     private static class A {}
 
     private static class B extends A {}
 
     private static class C extends B {}
+
+    @Dependent
+    static class DependentBean implements MyInterface {
+        static int destroyed = 0;
+
+        @PreDestroy
+        void destroy() {
+            destroyed++;
+        }
+    }
+
+    @Nested
+    @EnableWeld
+    class TestStream {
+
+        @WeldSetup
+        WeldInitiator weld = WeldInitiator.of(w);
+
+        @ParameterizedTest
+        @MethodSource("io.github.jhahnhro.enhancedcdi.util.BeanHelperTest#getArguments")
+        <T> void givenTypeAndQualifiers_whenSafeStream_thenAllInstancesAreReturned(final Type beanType,
+                                                                                   final Annotation[] qualifiers,
+                                                                                   final T[] expectedInstances) {
+            final Collection<T> actual = beanHelper.<T>safeStream(beanType, qualifiers)
+                    .map(BeanInstance::instance)
+                    .toList();
+            assertThat(actual).containsExactlyInAnyOrder(expectedInstances);
+        }
+
+        @Test
+        void givenType_whenSafeStreamIsClosed_thenAllDependentInstancesAreDestroyed() {
+            try (Stream<BeanInstance<MyInterface>> beanInstanceStream = beanHelper.safeStream(MyInterface.class)) {
+                final List<BeanInstance<MyInterface>> beanInstances = beanInstanceStream.toList();
+
+                // verify that terminal operation toList() does not close the stream
+                assertThat(AppScopedBean.destroyed).isZero();
+                assertThat(DependentBean.destroyed).isZero();
+
+                assertThat(beanInstances).hasSize(2);
+                beanInstances.forEach(BeanInstance::instance); // trigger lazy initialization
+
+                // verify that initialization has no side effects that trigger destruction
+                assertThat(AppScopedBean.destroyed).isZero();
+                assertThat(DependentBean.destroyed).isZero();
+            }
+
+            // verify that only the dependent beans were destroyed
+            assertThat(AppScopedBean.destroyed).isZero();
+            assertThat(DependentBean.destroyed).isEqualTo(1);
+        }
+
+
+    }
+
+    @Nested
+    @EnableWeld
+    class TestSelect {
+
+        @WeldSetup
+        WeldInitiator weld = WeldInitiator.of(w);
+
+        @Test
+        void givenUniqueBean_whenSelect_thenSucceed() {
+            final Type type = new TypeLiteral<List<Integer>>() {}.getType();
+            final List<Integer> selectedInstance = beanHelper.select(type);
+
+            assertThat(selectedInstance).isSameAs(integers);
+        }
+
+        @Test
+        void givenMultipleBeans_whenSelect_thenThrowAmbiguousResolutionException() {
+            final Type type = new TypeLiteral<Set<?>>() {}.getType();
+            assertThatThrownBy(() -> beanHelper.select(type)).isInstanceOf(AmbiguousResolutionException.class);
+        }
+
+        @Test
+        void givenNoBeans_whenSelect_thenThrowUnsatisfiedResolutionException() {
+            assertThatThrownBy(() -> beanHelper.select(Instant.class)).isInstanceOf(
+                    UnsatisfiedResolutionException.class);
+        }
+
+        @Test
+        void givenAppScopedBean_whenSelect_thenAlwaysReturnSameInstance() {
+            final Object instance1 = beanHelper.select(AppScopedBean.class);
+            final Object instance2 = beanHelper.select(AppScopedBean.class);
+
+            assertThat(instance1).isSameAs(instance2);
+        }
+
+        @Test
+        void givenDependentBean_whenSelect_thenAlwaysReturnNewInstance() {
+            final Object instance1 = beanHelper.select(DependentBean.class);
+            final Object instance2 = beanHelper.select(DependentBean.class);
+
+            assertThat(instance1).isNotEqualTo(instance2);
+        }
+
+        @Test
+        void whenSelect_thenDestroyingBeanHelperDestroysDependentBeans() {
+            final WeldInstance<BeanHelper> instance = weld.select(BeanHelper.class);
+
+            final BeanHelper localBeanHelper = instance.get();
+
+            localBeanHelper.select(AppScopedBean.class);
+            localBeanHelper.select(AppScopedBean.class);
+
+            localBeanHelper.select(DependentBean.class);
+            localBeanHelper.select(DependentBean.class);
+
+            instance.destroy(localBeanHelper);
+
+            assertThat(AppScopedBean.destroyed).isZero();
+            assertThat(DependentBean.destroyed).isEqualTo(2);
+        }
+    }
 }
