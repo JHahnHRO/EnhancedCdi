c1b52d31b155fff8a3bb35e40175ea6c20c62964
Johannes Hahn <johanneshahnrostock@googlemail.com>
2023-03-24 23:49:01 +0100

    minor refactoring - remove unused constructors

--- RabbitMQ-CDI-Bridge/core/src/main/java/io/github/jhahnhro/enhancedcdi/messaging/rpc/RpcNotActiveException.java
+++ RabbitMQ-CDI-Bridge/core/src/main/java/io/github/jhahnhro/enhancedcdi/messaging/rpc/RpcNotActiveException.java
@@ -4,12 +4,4 @@ public class RpcNotActiveException extends IllegalStateException {
     public RpcNotActiveException(String message) {
         super(message);
     }
-
-    public RpcNotActiveException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public RpcNotActiveException(Throwable cause) {
-        super(cause);
-    }
 }
