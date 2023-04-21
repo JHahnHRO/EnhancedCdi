02afd7868d7841527058d2a2e50651543b86fd30
Johannes Hahn <johanneshahnrostock@googlemail.com>
2023-04-21 23:51:19 +0200

    update pom.xml files - release 17 - new mockito version - automatic module names

--- RabbitMQ-CDI-Bridge/jsonb/src/main/java/module-info.java
+++ RabbitMQ-CDI-Bridge/jsonb/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module enhancedcdi.rabbitmqcdibridge.json {
     requires java.annotation;
     requires java.json.bind;
 
-    requires enhancedcdi.rabbitmqcdibridge;
+    requires enhancedcdi.rabbitmqcdibridge.core;
     requires com.rabbitmq.client;
 
     exports io.github.jhahnhro.enhancedcdi.serialization.json;
