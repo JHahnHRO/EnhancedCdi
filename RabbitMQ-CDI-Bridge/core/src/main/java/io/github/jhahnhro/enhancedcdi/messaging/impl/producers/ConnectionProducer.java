78f58d987f5b6fd62521e4d63d9b30a5d2733f9f
Johannes Hahn <johanneshahnrostock@googlemail.com>
2023-10-11 21:56:58 +0200

    improve logging

--- RabbitMQ-CDI-Bridge/core/src/main/java/io/github/jhahnhro/enhancedcdi/messaging/impl/producers/ConnectionProducer.java
+++ RabbitMQ-CDI-Bridge/core/src/main/java/io/github/jhahnhro/enhancedcdi/messaging/impl/producers/ConnectionProducer.java
@@ -1,11 +1,12 @@
 package io.github.jhahnhro.enhancedcdi.messaging.impl.producers;
 
+import static java.util.Objects.requireNonNullElse;
+
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.lang.System.Logger.Level;
 import java.util.concurrent.TimeoutException;
-import java.util.function.BiConsumer;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.CreationException;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
@@ -20,33 +21,43 @@ class ConnectionProducer {
 
     private static final System.Logger LOG = System.getLogger(ConnectionProducer.class.getCanonicalName());
 
-    @Produces
-    @ApplicationScoped
-    Connection produceConnection(Configuration configuration) {
-        return newConnection(configuration);
-    }
-
-    void disposeConnection(@Disposes Connection connection) throws IOException {
-        LOG.log(Level.INFO, "Shutting down connection to RabbitMQ broker...");
-        try {
-            connection.close();
-        } catch (AlreadyClosedException sse) {
-            LOG.log(Level.DEBUG, "Connection to RabbitMQ broker was already shut down");
+    private static void logFailure(int nr, Exception ex, String formatString) {
+        final String msg = String.format(formatString, nr,
+                                         requireNonNullElse(ex.getMessage(), ex.getClass().getSimpleName()));
+        if (LOG.isLoggable(Level.DEBUG)) {
+            LOG.log(Level.DEBUG, msg, ex);
+        } else {
+            LOG.log(Level.WARNING, msg + " Set log level to DEBUG for full stack trace.");
         }
     }
 
-    private Connection newConnection(Configuration configuration) {
+    @Produces
+    @ApplicationScoped
+    Connection produceConnection(Configuration configuration) throws InterruptedException, TimeoutException {
+        return newConnection(configuration);
+    }
+
+    void disposeConnection(@Disposes Connection connection) {
+        LOG.log(Level.INFO, "Shutting down connection to RabbitMQ broker...");
+        try {
+            connection.close();
+            LOG.log(Level.INFO, "Connection to RabbitMQ broker successfully shut down.");
+        } catch (AlreadyClosedException sse) {
+            LOG.log(Level.DEBUG, "Connection to RabbitMQ broker was already shut down");
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Connection newConnection(Configuration configuration) throws InterruptedException, TimeoutException {
         final Connection connection;
         try {
             LOG.log(Level.INFO, "Opening connection to RabbitMQ broker...");
             connection = newConnectionWithRetry(configuration);
             LOG.log(Level.INFO, "Connection to RabbitMQ broker successfully opened.");
-        } catch (TimeoutException e) {
-            throw new CreationException("Connection to RabbitMQ broker could not be established", e);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new CreationException(
-                    "Connection to RabbitMQ broker was not established, because the thread was interrupted", e);
+        } catch (TimeoutException | InterruptedException e) {
+            LOG.log(Level.ERROR, "Connection to RabbitMQ broker was not established", e);
+            throw e;
         }
 
         connection.addShutdownListener(shutdownSignal -> {
@@ -63,10 +74,10 @@ class ConnectionProducer {
             throws TimeoutException, InterruptedException {
 
         final Retry retry = configuration.initialConnectionRetry();
-        final String msg = getMsgForFailedAttempt(retry);
-        final BiConsumer<Integer, Exception> logFailures = (nr, e) -> LOG.log(Level.INFO,
-                                                                              msg.formatted(nr, e.getMessage()));
-        return retry.call(() -> configuration.connectionFactory().newConnection(), logFailures);
+        final String formatString = getMsgForFailedAttempt(retry);
+
+        return retry.call(() -> configuration.connectionFactory().newConnection(),
+                          (attemptNr, exception) -> logFailure(attemptNr, exception, formatString));
     }
 
     private String getMsgForFailedAttempt(Retry retry) {
